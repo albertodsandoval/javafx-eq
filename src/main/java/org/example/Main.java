4a4ea09b9d32160aa48e8e95c02bfd23770c9ce8
AlbertoSandovalCSUN <141599770+AlbertoSandovalCSUN@users.noreply.github.com>
2024-12-27 20:41:43 -0800

    slider

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,33 +1,49 @@
 package org.example;
 
 import javafx.application.Application;
+import javafx.geometry.Orientation;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        // creating group
         Group root = new Group();
+
+        // create a Scene
         Scene scene = new Scene(root, 600, 400);
 
-        // set Scene to the stage
+        // add Scene to the frame
         stage.setScene(scene);
 
-        // set title for the frame
+        // set title of the frame
         stage.setTitle("Slider Sample");
 
-        // create slider
-        Slider slider = new Slider();
+        // Creates a slider
+        Slider slider = new Slider(-10, 10, 0);
 
-        // add slider to the frame
+        // enable the marks
+        slider.setShowTickMarks(true);
+
+        // enable the Labels
+        slider.setShowTickLabels(true);
+
+        // set Major tick unit
+        slider.setMajorTickUnit(1);
+
+        // sets the value of the property
+        // blockIncrement
+        slider.setBlockIncrement(0.5f);
+
         root.getChildren().add(slider);
 
+        // display
         stage.show();
     }
 
