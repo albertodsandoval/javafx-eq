915d315e2481f1a02a29bf90fc2486efb53d3c89
AlbertoSandovalCSUN <141599770+AlbertoSandovalCSUN@users.noreply.github.com>
2024-12-28 15:59:32 -0800

    10 sliders side by side

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,49 +1,46 @@
 package org.example;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
-        Group root = new Group();
-
-        // create a Scene
-        Scene scene = new Scene(root, 600, 400);
-
-        // add Scene to the frame
+        HBox layout = new HBox();
+        layout.setSpacing(10);
+        Insets Insets = new Insets(50);
+        layout.setPadding(Insets);
+        for(int i = 0; i<11;i++){
+            Slider slider = new Slider();
+            slider.setOrientation(Orientation.VERTICAL);
+            slider.setMax(10);
+            slider.setMin(-10);
+            slider.setValue(0);
+            slider.setMajorTickUnit(1);
+            slider.setBlockIncrement(0.5f);
+            slider.setShowTickMarks(true);
+            slider.setShowTickLabels(true);
+            slider.setSnapToTicks(true);
+            layout.getChildren().add(slider);
+        }
+        Scene scene = new Scene(layout);
+        stage.setTitle("10 Sliders");
         stage.setScene(scene);
-
-        // set title of the frame
-        stage.setTitle("Slider Sample");
-
-        // Creates a slider
-        Slider slider = new Slider(-10, 10, 0);
-
-        // enable the marks
-        slider.setShowTickMarks(true);
-
-        // enable the Labels
-        slider.setShowTickLabels(true);
-
-        // set Major tick unit
-        slider.setMajorTickUnit(1);
-
-        // sets the value of the property
-        // blockIncrement
-        slider.setBlockIncrement(0.5f);
-
-        root.getChildren().add(slider);
-
-        // display
+        stage.setHeight(500);
+        stage.setWidth(700);
         stage.show();
     }
 
